e057e828fd635e38aad00f4775342df1030a30fb
vigneshxcode1 <vigneshxcode@gmail.com>
2025-11-05 20:43:40 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "../css/Home.css";
 
 const API_KEY = "24530631";
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
